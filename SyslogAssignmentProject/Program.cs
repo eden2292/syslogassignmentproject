2a752769fde55407c867cb2e2307fde6708b9fbf
eden2292 <kp798@student.aru.ac.uk>
2023-11-02 14:45:25 +0000

    Added syncfusion for treebview.

--- SyslogAssignmentProject/Program.cs
+++ SyslogAssignmentProject/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SyslogAssignmentProject.Data;
+using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
