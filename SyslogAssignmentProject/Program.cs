b53dac00814621b3fc015051ced20fdccd770bcf
eden2292 <kp798@student.aru.ac.uk>
2023-11-03 09:38:01 +0000

    Creating dynamic tree view, fixed issue with code block.

--- SyslogAssignmentProject/Program.cs
+++ SyslogAssignmentProject/Program.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SyslogAssignmentProject.Data;
-using Syncfusion.Blazor;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
