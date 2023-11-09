24560890dfd8edfc4c3aad22e996bc7758fec374
AdamC33 <ac1904@student.aru.ac.uk>
2023-11-09 14:29:59 +0000

    Added image filepath to radio class

--- SyslogAssignmentProject/Classes/Radio.cs
+++ SyslogAssignmentProject/Classes/Radio.cs
@@ -7,6 +7,13 @@ public class Radio
   public string Name { get; set; }
   public string IPAddress { get; set; }
   public string TransportProtocol { get; set; }
+  private string PathOfImage
+  {
+    get
+    {
+      return $"wwwroot/{Name}.jpg";
+    }
+  }
 
   public Radio(string _name, string _ipAddress, string _transportProtocol)
   {
