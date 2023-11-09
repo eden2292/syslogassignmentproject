06f465fb31008fb5179b0c074ddfe4799888f7ce
AdamC33 <ac1904@student.aru.ac.uk>
2023-11-09 14:22:35 +0000

    Fixed radio display names in navbar

--- SyslogAssignmentProject/Services/BackgroundRunner.cs
+++ SyslogAssignmentProject/Services/BackgroundRunner.cs
@@ -52,13 +52,13 @@ namespace SyslogAssignmentProject.Services
         if (_udpListener.EarsFull || _udpListener.TokenToStopListening.Token.IsCancellationRequested)
         {
           _listeningOnTcpAndUdp.Add(_udpListener);
-          RadioStore.Add(new Radio("T6S3", "tempIP", "UDP"));
+          RadioStore.Add(new Radio("T6S3", _udpListener.SourceInformation.Address.ToString(), "UDP"));
           _udpListener = new UdpSyslogReceiver();
         }
         if (_tcpListener.EarsFull || _tcpListener.TokenToStopListening.Token.IsCancellationRequested)
         {
           _listeningOnTcpAndUdp.Add(_tcpListener);
-          RadioStore.Add(new Radio("T6S3", "tempIP", "TCP"));
+          RadioStore.Add(new Radio("T6S3", _tcpListener.SourceInformation.Address.ToString(), "TCP"));
           _tcpListener = new TcpSyslogReceiver();
         }
         DuplicateRemover(RadioStore);
