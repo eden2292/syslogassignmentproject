70d046e0e0b7b8eb85032fa45ab3ce4995bcc1e4
AdamC33 <ac1904@student.aru.ac.uk>
2023-11-10 18:52:54 +0000

    Updated live feed exporter to export each radio's messages into their own file instead of one big file

--- SyslogAssignmentProject/Classes/LogExport.cs
+++ SyslogAssignmentProject/Classes/LogExport.cs
@@ -11,39 +11,45 @@ namespace SyslogAssignmentProject.Classes
 {
   public class LogExport
   {
-    private static StreamWriter s_fileWriter;
-    private static int s_filescounter = 0;
     public static void s_export(string? ipAddress)
     {
+      // A dictionary of streamwriters, indexed by IP address string
+      Dictionary<string, StreamWriter> streamWriterDict = new Dictionary<string, StreamWriter>();
       // If ipAddress is null, export all messages regardless of radio
-      string _ipAddressFilename = "anyIP";
+
       string _formattedDateTime = DateTime.Now.ToString("yyyyMMddTHHmmss");
-      if (ipAddress != null)
-        _ipAddressFilename = ipAddress.Replace(":", "_"); // Windows filenames do not allow for colons
-
-      string _logFileName = $"{_ipAddressFilename}_{_formattedDateTime}_{s_filescounter}.txt";
-
-      s_fileWriter = new StreamWriter(_logFileName, true);
 
       foreach(SyslogMessage message in S_LiveFeedMessages.SyslogMessageList)
       {
-        if (ipAddress == null || message.SenderIP == ipAddress)
+        if(ipAddress == null || message.SenderIP == ipAddress)
         {
-          s_fileWriter.WriteLine(message.FullMessage);
+          if(!streamWriterDict.ContainsKey(message.SenderIP))
+          {
+            string ipAddressFilename = message.SenderIP.Replace(":", "_"); // Windows filenames do not allow for colons
+            string logFileName = $"{ipAddressFilename}_{_formattedDateTime}.txt";
+            streamWriterDict.Add(message.SenderIP, new StreamWriter(logFileName));
+          }
+
+          streamWriterDict[message.SenderIP].WriteLine(message.FullMessage);
         }
       }
 
-      s_fileWriter?.Flush();
-      s_fileWriter?.Close();
       string zipPath = @".\Logs.zip";
-      string fileToAdd = @$"\{_logFileName}";
 
-      using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+      foreach (StreamWriter streamWriter in streamWriterDict.Values)
       {
-        string fileName = Path.GetFileName(fileToAdd);
-        archive.CreateEntryFromFile(_logFileName, fileName);
+        streamWriter?.Flush();
+        streamWriter?.Close();
+        string logFileName = (streamWriter.BaseStream as FileStream).Name;
+        string fileToAdd = @$"\{logFileName}";
+
+        using(ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+        {
+          string fileName = Path.GetFileName(fileToAdd);
+          archive.CreateEntryFromFile(logFileName, fileName);
+        }
+        File.Delete(logFileName);
       }
-      File.Delete(_logFileName);
     }
   }
 }
