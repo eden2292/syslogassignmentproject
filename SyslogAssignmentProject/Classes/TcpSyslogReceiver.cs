13cfab64c55ed7525dd82311437fea1faec4507d
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2023-11-12 16:05:18 +0000

    Removed it :( but its commented out because I contest against the silliness.

--- SyslogAssignmentProject/Classes/TcpSyslogReceiver.cs
+++ SyslogAssignmentProject/Classes/TcpSyslogReceiver.cs
@@ -75,8 +75,8 @@ namespace SyslogAssignmentProject.Classes
         {
           while ((_bytesRead = await _syslogMessageStream.ReadAsync(_buffer, 0, _buffer.Length)) != 0)
           {
-            if ((S_ListeningOptions.Equals("Both") || S_ListeningOptions.Equals("TCP")) && S_ReceivingIpAddress.Equals(SourceIpAddress.Address.ToString()))
-            {
+            if ((S_ListeningOptions.Equals("Both") || S_ListeningOptions.Equals("TCP"))) //&& S_ReceivingIpAddress.Equals(SourceIpAddress.Address.ToString())
+          {
               _formattedMessage = new SyslogMessage(SourceIpAddress.Address.ToString(), DateTime.Now,
                 Encoding.ASCII.GetString(_buffer, 0, _bytesRead), "TCP");
               if (((_formattedMessage.ParseMessage() & SyslogMessage.ParseFailure.Priority) != SyslogMessage.ParseFailure.Priority)
