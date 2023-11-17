2191e0ce16f25354e53675f9b9cacbe63e416f58
Eden Peacock <KP798@student.aru.ac.uk>
2023-11-17 21:03:43 +0000

    Hiding function added - works with TCP. UDP radio connections need to be addressed to make it also work with UDP.

--- SyslogAssignmentProject/Classes/Radio.cs
+++ SyslogAssignmentProject/Classes/Radio.cs
@@ -4,17 +4,18 @@
 /// </summary>
 public class Radio
 {
+  public Guid Id { get; set; }
   public string Name { get; set; }
   public string IPAddress { get; set; }
   public string TransportProtocol { get; set; }
   public string PathOfImage { get; set; }
-
   public string HexColour { get; set; }
   public int PortNumber { get; set; }
   public bool Hidden { get; set; } = false;
 
   public Radio(string name, string ipAddress, int portNumber, string transportProtocol)
   {
+    Id = Guid.NewGuid();
     Name = name;
     IPAddress = ipAddress;
     TransportProtocol = transportProtocol;
--- SyslogAssignmentProject/Classes/RadioInjection.cs
+++ SyslogAssignmentProject/Classes/RadioInjection.cs
@@ -2,6 +2,17 @@
 {
   public class RadioInjection
   {
-    public Radio radio { get; set; } = new();
+    private readonly RadioListServicer _radioListServicer;
+
+    public RadioInjection(RadioListServicer radioListServicer)
+    {
+      _radioListServicer = radioListServicer;
+    }
+    public Radio radio { get; set; }
+
+    public void UpdateRadioHiddenValue()
+    {
+      _radioListServicer.RadioStore.Single(x => x.Id == radio.Id).Hidden = radio.Hidden;
+    }
   }
 }
