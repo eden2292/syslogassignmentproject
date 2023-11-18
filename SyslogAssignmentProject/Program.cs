7774c5c0204450c5461f4481753c1433866ef575
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2023-11-18 21:15:19 +0000

    notes on what remains.

--- SyslogAssignmentProject/Program.cs
+++ SyslogAssignmentProject/Program.cs
@@ -35,6 +35,17 @@ app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-BackgroundRunner backgroundRunner = app.Services.GetRequiredService<BackgroundRunner>();
+BackgroundRunner _backgroundRunner = app.Services.GetRequiredService<BackgroundRunner>();
 
 app.Run();
+
+/* Tasks that need to be done to fulfil requirements on this branch.
+ * Radio page needs to fulfil requirements, needs to display protocol, ip, ip type, potentially port (see what Sam says)
+ * Hiding radios on this version
+ * Deleting Radios on this version
+ * Suppressing warnings (unsure how)
+ * Adding log export button to each radio page (back end in place buttons needed)
+ * Potentially adding local delete on each radio page (back end needs to be added for this).
+ * Error occurs when more than one radio is added, live feed messages for that radio does not seem to be displayed. Fix unneeeded as likely due to how messages for that radio
+ * are being processed which will change.
+ * Define what a radio is.*/
